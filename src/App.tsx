1337a50c68ebfef7b79818de9b228d950a6800d2
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2025-05-28 22:15:53 -0300

    feat: implements shadcn cli

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <p className="text-3xl text-red-600">Teste</p>
+      <Button className="bg-amber-300">botão</Button>
     </>
   );
 }
